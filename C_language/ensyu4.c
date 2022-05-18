7a750018372a7931c9b1efaaf3fa060285073e33
Yosuke <yousuke@ageet.com>
2022-05-18 21:17:02 +0900

    ensyu4

--- C_language/ensyu4.c
+++ C_language/ensyu4.c
@@ -10,15 +10,15 @@ int main(void){
   hword key_ptr;
   ptr = (hword*)0x04000000;
   *ptr = 0x0F03;
-  button = 0x0008;
+  button = 0x0002;
   key = (hword*)0x04000130;
-  
+
   while(1)[
     key_ptr = *key;
     if ((key_ptr&button) != button) {
-      color = ;
+      color = 0x7FFF;
     } else {
-      color = ;
+      color = 0x7FE0;
     }
 
     ptr = (hword*)VRAM;
